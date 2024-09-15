f803dfb1386ff856a6207753a1f20ceefeeb3748
Igor Ximenes <igor.ximenes@ifood.com.br>
2024-09-14 21:06:24 -0300

    adding subject module started function

--- internal/api/subject.go
+++ internal/api/subject.go
@@ -1,59 +1,61 @@
 package api
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/igorXimeness/educ-off-api/internal/model"
 	"github.com/igorXimeness/educ-off-api/internal/service"
 	"github.com/labstack/echo/v4"
 )
-
-type Subject interface {
-	fetchModules(ctx context.Context, subject_name string) ([]model.Modules, error)
-}
-
 
 type SubjectAPI struct {
 	subjectService service.SubjectService
 }
 
 func NewSubjectAPI(subjectService service.SubjectService) SubjectAPI {
-    return SubjectAPI {
-        subjectService: subjectService,
-    }
+	return SubjectAPI{
+		subjectService: subjectService,
+	}
 }
-
 
 func (api SubjectAPI) Register(e *echo.Echo) {
 	v1 := e.Group("v1")
-    v1.GET("/modules/:subject_name", api.FetchModules)
+	v1.GET("/modules/:subject_name", api.FetchModules)
 	v1.GET("/subjects", api.FetchSubjects)
-    v1.PUT("/modules/:module_id/finish", api.FinishModule) 
+	v1.GET("/subjects-with-done-modules", api.FetchSubjectsWithDoneModules) // Nova rota
+	v1.PUT("/modules/:module_id/finish", api.FinishModule)
 }
 
 func (api SubjectAPI) FetchSubjects(c echo.Context) error {
-    subjects, err := api.subjectService.FetchSubjects(c.Request().Context())
+	subjects, err := api.subjectService.FetchSubjects(c.Request().Context())
 
-    if err != nil {
-        return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-    }
-	
-    return c.JSON(http.StatusOK, subjects)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, subjects)
 }
 
 func (api SubjectAPI) FetchModules(c echo.Context) error {
-    subjectName := c.Param("subject_name")  // Corrigindo a captura do parâmetro de rota
+	subjectName := c.Param("subject_name") // Captura o parâmetro de rota
 
-    modules, err := api.subjectService.FetchModules(c.Request().Context(), subjectName)
+	modules, err := api.subjectService.FetchModules(c.Request().Context(), subjectName)
 
-    if err != nil {
-        return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
-    }
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+	}
 
-    return c.JSON(http.StatusOK, modules)
+	return c.JSON(http.StatusOK, modules)
 }
 
+func (api SubjectAPI) FetchSubjectsWithDoneModules(c echo.Context) error {
+	subjects, err := api.subjectService.FetchSubjectsWithDoneModules(c.Request().Context())
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, subjects)
+}
 
 func (api SubjectAPI) FinishModule(c echo.Context) error {
 	moduleID := c.Param("module_id")
@@ -61,5 +63,5 @@ func (api SubjectAPI) FinishModule(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
 	}
-	return c.NoContent(http.StatusNoContent) // Retorna apenas um status 204 No Content
+	return c.NoContent(http.StatusNoContent)
 }
--- internal/service/subject.go
+++ internal/service/subject.go
@@ -2,35 +2,39 @@ package service
 
 import (
 	"context"
+
 	"github.com/igorXimeness/educ-off-api/internal/model"
 )
 
 type SubjectService struct {
-	userRepository SubjectRepository
+	subjectRepository SubjectRepository
 }
 
 type SubjectRepository interface {
-	FetchModules(context.Context, string) ([]model.Modules, error)  
-	FetchSubjects(context.Context) ([]model.Subject, error) 
-	FinishModule(context.Context, string) error 
+	FetchModules(context.Context, string) ([]model.Modules, error)
+	FetchSubjects(context.Context) ([]model.Subject, error)
+	FetchSubjectsWithDoneModules(context.Context) ([]model.Subject, error) // Novo método
+	FinishModule(context.Context, string) error
 }
 
-func NewSubjectService(userRepository SubjectRepository) SubjectService {
-	return SubjectService {
-		userRepository: userRepository,
+func NewSubjectService(subjectRepository SubjectRepository) SubjectService {
+	return SubjectService{
+		subjectRepository: subjectRepository,
 	}
 }
 
-
-func(s SubjectService) FetchSubjects(ctx context.Context) ([]model.Subject, error) {
-	return s.userRepository.FetchSubjects(ctx)
+func (s SubjectService) FetchSubjects(ctx context.Context) ([]model.Subject, error) {
+	return s.subjectRepository.FetchSubjects(ctx)
 }
 
 func (s SubjectService) FetchModules(ctx context.Context, subject string) ([]model.Modules, error) {
-    return s.userRepository.FetchModules(ctx, subject)
+	return s.subjectRepository.FetchModules(ctx, subject)
 }
 
+func (s SubjectService) FetchSubjectsWithDoneModules(ctx context.Context) ([]model.Subject, error) {
+	return s.subjectRepository.FetchSubjectsWithDoneModules(ctx) // Chama o novo método no repositório
+}
 
 func (s SubjectService) FinishModule(ctx context.Context, moduleID string) error {
-	return s.userRepository.FinishModule(ctx, moduleID)
+	return s.subjectRepository.FinishModule(ctx, moduleID)
 }
