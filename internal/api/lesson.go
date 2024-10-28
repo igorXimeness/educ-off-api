4bc8f3ec0162b481f94feaeb3652bc8796d66b1f
Igor Ximenes <igorximenes27@gmail.com>
2024-10-28 00:16:37 -0300

    fea: adding pdf route

--- internal/api/lesson.go
+++ internal/api/lesson.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"context"
+
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/igorXimeness/educ-off-api/internal/model"
 	"github.com/igorXimeness/educ-off-api/internal/service"
@@ -28,6 +31,7 @@ func NewLessonAPI(lessonService service.LessonService) LessonAPI {
 func (api LessonAPI) Register(e *echo.Echo) {
 	v1 := e.Group("v1")
 	v1.GET("/modules/:module_name/lesson", api.FetchLesson)
+	v1.GET("/pdfs/:subject_name", api.FetchPDF) 
 }
 
 
@@ -39,4 +43,29 @@ func (api LessonAPI) FetchLesson(c echo.Context) error {
         return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
     }
     return c.JSON(http.StatusOK, lesson)
+}
+
+
+
+func (api LessonAPI) FetchPDF(c echo.Context) error {
+    subjectName := c.Param("subject_name")
+    
+    // Caminho para o diretório "assets" a partir do diretório de trabalho
+    pdfPath := filepath.Join("assets", subjectName+".pdf")
+    
+    // Verifica se o arquivo existe
+    if _, err := os.Stat(pdfPath); err != nil {
+        if os.IsNotExist(err) {
+            return c.JSON(http.StatusNotFound, map[string]interface{}{
+                "error": "File not found: " + pdfPath,
+            })
+        }
+        // Retornar outros erros, caso ocorra algum problema inesperado
+        return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+            "error": "Error checking file: " + err.Error(),
+        })
+    }
+    
+    // Retornando o PDF como resposta
+    return c.File(pdfPath)
 }
